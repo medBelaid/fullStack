4d94a03fd852a13acd1de8e94e8100d5de904b3c
medBelaid <medbbelaid@gmail.com>
2016-06-29 13:50:34 +0100

    template JeBouquine with bootstrap

---
+++ js/controllers/p4Ctrl.js
@@ -0,0 +1,8 @@
+
+    Puissance4app.controller('Ctrl1', ['$scope','timeDiffService', function($scope,timeDiffService) {
+
+        $scope.person = [
+            {name:"med",email:"medbbe@gmail.com",ville:"Sahline"},
+            {name:"amal",email:"amalbbe@gmail.com",ville:"Sousse"}
+        ]
+        }]);
